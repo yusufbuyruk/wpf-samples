19c0430359faa053aeb3fb5f6bbf0b109e32bd53
Yusuf Buyruk <51216067+yusufbuyruk@users.noreply.github.com>
2023-12-26 12:10:47 -0500

    Update 1226-1210

---
+++ WPF UI Academy/Flat-Login-Page/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flat_Login_Page
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
+        }
+
+        private bool _isMaximized = false;
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (_isMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1024;
+                    this.Height = 720;
+
+                    _isMaximized = false;
+                }
+
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    _isMaximized = true;
+                }
+
+            }
+        }
+    }
+}
